99c4e9a31245dfcd543e9d79abe637c42e93c95b
Deanish <m.a.deanish@gmail.com>
2023-03-16 17:39:22 +0000

    Drupal updated to 10.0.5

---
+++ web/sites/default/files/php/twig/64131792a71c5_update-report.html.twig_v6m7WW4l4r8Iw5K7BlTezCEIc/0NKPwqaSAco0wajzNzhSDrpOwNkwd1mA9BfbHxTnniM.php
@@ -0,0 +1,118 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable9/templates/admin/update-report.html.twig */
+class __TwigTemplate_f4caa58af08409566f856e86b537aa69 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 16
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["last_checked"] ?? null), 16, $this->source), "html", null, true);
+        echo "
+
+";
+        // line 18
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["project_types"] ?? null));
+        $context['_iterated'] = false;
+        foreach ($context['_seq'] as $context["_key"] => $context["project_type"]) {
+            // line 19
+            echo "  <h3>";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["project_type"], "label", [], "any", false, false, true, 19), 19, $this->source), "html", null, true);
+            echo "</h3>
+  ";
+            // line 20
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["project_type"], "table", [], "any", false, false, true, 20), 20, $this->source), "html", null, true);
+            echo "
+";
+            $context['_iterated'] = true;
+        }
+        if (!$context['_iterated']) {
+            // line 22
+            echo "  <p>";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["no_updates_message"] ?? null), 22, $this->source), "html", null, true);
+            echo "</p>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['project_type'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable9/templates/admin/update-report.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  61 => 22,  54 => 20,  49 => 19,  44 => 18,  39 => 16,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable9/templates/admin/update-report.html.twig", "/var/www/html/alpha/web/core/themes/stable9/templates/admin/update-report.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 18);
+        static $filters = array("escape" => 16);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
